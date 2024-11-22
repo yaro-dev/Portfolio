17a72bc457828c6cde41aec11726acf9a62eec22
yaro-dev <yarowebdev@gmail.com>
2024-11-22 15:36:24 -0600

    CV added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,7 +39,7 @@ export default function About() {
                     <div className="about__buttons">
                         <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}
                             onHoverStart={e => { }}
-                            onHoverEnd={e => { }} download="" href="assets/pdf/Yaro-Cv.pdf" className="button button--flex">
+                            onHoverEnd={e => { }} download="" href="/pdf/Yair_Alvarado_Colin_CV_en.pdf" className="button button--flex">
                             Download CV<i className="uil uil-download-alt"></i>
                         </motion.a>
                     </div>
